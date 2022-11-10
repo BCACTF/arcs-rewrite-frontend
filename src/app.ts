d66485fb049afebfdf1a9dc5d3535f2209a82055
kna27 <krisharora27@gmail.com>
2022-11-10 14:32:57 -0500

    Create node and ts project

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    return console.log(`Server started at http://localhost:${port}`);
+});
