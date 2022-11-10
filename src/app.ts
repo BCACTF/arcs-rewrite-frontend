96d026972c51a83cf9dd06f33cf3ca7d4deca8d2
kna27 <krisharora27@gmail.com>
2022-11-10 16:34:26 -0500

    Set up routes, env vars, and hbs partials

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express from 'express';
-const app = express();
-const port = 3000;
+import path from 'path';
+import logger from 'morgan';
+import routes from './routes';
+import hbs from 'hbs';
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "../", "views"));
+app.set("view engine", "hbs");
+hbs.registerPartials(path.join(__dirname, "../", "views/partials"));
+
+app.use(routes);
+const port = 3000;
 
 app.listen(port, () => {
     return console.log(`Server started at http://localhost:${port}`);
