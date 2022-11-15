89ab7fda939c8d6c1001a4916c29265c148eac21
kna27 <krisharora27@gmail.com>
2022-11-15 15:54:37 -0500

    Add password requirements and confirmation

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -13,6 +13,35 @@ router.get('/register', function (req, res) {
 });
 
 router.post('/register', async function (req, res) {
+    if (req.body.password != req.body.password2) {
+        res.render('register', {
+            eventname: process.env.EVENT_NAME,
+            error: "Passwords do not match"
+        });
+    }
+    let passwordMeetsRequirements = true;
+    if (req.body.password.length < 8) {
+        passwordMeetsRequirements = false;
+    }
+    if (!req.body.password.match(/[a-z]/)) {
+        passwordMeetsRequirements = false;
+    }
+    if (!req.body.password.match(/[A-Z]/)) {
+        passwordMeetsRequirements = false;
+    }
+    if (!req.body.password.match(/[0-9]/)) {
+        passwordMeetsRequirements = false;
+    }
+    if (!req.body.password.match(/[^a-zA-Z0-9]/)) {
+        passwordMeetsRequirements = false;
+    }
+    if (!passwordMeetsRequirements) {
+        return res.render('register', {
+            eventname: process.env.EVENT_NAME,
+            error: "Password does not meet requirements"
+        });
+    }
+
     let userExists = await queries.checkIfExistingNameorEmail(req.body.email, req.body.username);
     if (!userExists) {
         log.debug(`Attemping to register user ${req.body.username} ${req.body.email}`);
@@ -24,10 +53,10 @@ router.post('/register', async function (req, res) {
     } else {
         log.info(`Attempted registration of existing username/email: ${req.body.username} ${req.body.email}`);
         res.render('register', {
+            eventname: process.env.EVENT_NAME,
             error: "Username or email already in use, <a href='/login'>Log in instead?</a>"
         });
     }
 });
-
 
 export default router;
