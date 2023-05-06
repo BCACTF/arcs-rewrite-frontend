07992b9900566dbd33144bd2966dda2ab3a39821
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2023-05-06 17:41:10 -0400

    Temp clean-up and fixing the type errors.
    (This needs to be gone back over and made logically correct though.)

--- pages/api/new-user.ts
+++ pages/api/new-user.ts
@@ -9,7 +9,7 @@ const handler: NextApiHandler = async (req, res) =>  {
     const { email, name } = { email: token?.email, name: JSON.parse(req.body).name };
     console.log({email, name})
     if (email && name) {
-        await addUser({ email, name, password: "", eligible: true });
+        await addUser({ email, name, auth: { __type: "pass", password: "" }, eligible: true });
     }
     res.send("bloop");
 };
