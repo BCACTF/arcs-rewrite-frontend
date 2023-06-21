1c56a6828b8b75ab174bdba9ac9c3cef8f2a46eb
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2023-06-21 08:40:30 -0400

    Update sync endpoint for new webhook format

--- pages/api/sync.ts
+++ pages/api/sync.ts
@@ -1,10 +1,40 @@
 import { NextApiHandler } from "next";
 import { timingSafeEqual } from "crypto";
 import { webhookToken } from "auth/challenges";
-import { syncAllChalls } from "database/challs";
-import { syncAllTeams } from "database/teams";
-import { syncAllUsers } from "database/users";
+import { syncAllChalls, syncChall } from "database/challs";
+import { syncAllTeams, syncTeam } from "database/teams";
+import { syncAllUsers, syncUser } from "database/users";
 import { syncSolves } from "database/solves";
+import { ChallId, TeamId, UserId, challIdFromStr, teamIdFromStr, userIdFromStr } from "cache/ids";
+
+
+enum SyncType {
+    ALL,
+    SOLVES,
+
+    ALL_CHALLS,
+    ALL_TEAMS,
+    ALL_USERS,
+
+    CHALL,
+    TEAM,
+    USER,
+}
+
+type SyncInfo = {
+    __type: SyncType.ALL | SyncType.SOLVES | SyncType.ALL_CHALLS | SyncType.ALL_TEAMS | SyncType.ALL_USERS,
+} | {
+    __type: SyncType.CHALL,
+    id: ChallId,
+} | {
+    __type: SyncType.TEAM,
+    id: TeamId,
+} | {
+    __type: SyncType.USER,
+    id: UserId,
+};
+
+const uuidIsntAll0s = (uuid: string) => uuid.split("").filter(c => !isNaN(parseInt(c, 16))).every(c => c !== "0");
 
 const handler: NextApiHandler = async (req, res) =>  {
     console.log("request recieved");
@@ -38,22 +68,117 @@ const handler: NextApiHandler = async (req, res) =>  {
         return;
     }
     
+    let syncType: SyncInfo;
+
     try {
-        await Promise.all([
-            syncAllChalls(),
-            syncAllTeams(),
-            syncAllUsers(),
-            syncSolves(),
-        ]);
-        
-        res.statusMessage = "Challenge successfully synced";
-        res.status(200);
-        res.end();
+        const body = req.body as { __type: string, id?: string };
+
+        switch (body.__type) {
+            case "all":
+                syncType = {
+                    __type: SyncType.ALL
+                };
+                break;
+            case "solves":
+                syncType = {
+                    __type: SyncType.SOLVES
+                };
+                break;
+            case "user":
+                if (!body.id) throw new Error("no id provided");
+                if (uuidIsntAll0s(body.id)) {
+                    const id = userIdFromStr(body.id);
+                    if (!id) throw new Error("bad id format");
+                    syncType = {
+                        __type: SyncType.USER,
+                        id,
+                    };
+                } else {
+                    syncType = {
+                        __type: SyncType.ALL_USERS,
+                    };
+                }
+                break;
+            case "team":
+                if (!body.id) throw new Error("no id provided");
+                if (uuidIsntAll0s(body.id)) {
+                    const id = teamIdFromStr(body.id);
+                    if (!id) throw new Error("bad id format");
+                    syncType = {
+                        __type: SyncType.TEAM,
+                        id,
+                    };
+                } else {
+                    syncType = {
+                        __type: SyncType.ALL_TEAMS,
+                    };
+                }
+                break;
+            case "chall":
+                if (!body.id) throw new Error("no id provided");
+                if (uuidIsntAll0s(body.id)) {
+                    const id = challIdFromStr(body.id);
+                    if (!id) throw new Error("bad id format");
+                    syncType = {
+                        __type: SyncType.CHALL,
+                        id,
+                    };
+                } else {
+                    syncType = {
+                        __type: SyncType.ALL_CHALLS,
+                    };
+                }
+                break;
+            default:
+                throw new Error("Unknown sync type");
+        }
     } catch (e) {
         res.status(400);
         res.end();
+        return;
     }
-    
+
+    try {
+        let promises: ((() => Promise<unknown>) | Promise<unknown>)[];
+
+        switch (syncType.__type) {
+            case SyncType.ALL:
+                promises = [syncSolves, syncAllChalls, syncAllTeams, syncAllUsers];
+                break;
+            case SyncType.SOLVES:
+                promises = [syncSolves];
+                break;
+            case SyncType.ALL_CHALLS:
+                promises = [syncAllChalls];
+                break;
+            case SyncType.ALL_TEAMS:
+                promises = [syncAllTeams];
+                break;
+            case SyncType.ALL_USERS:
+                promises = [syncAllUsers];
+                break;
+            case SyncType.CHALL:
+                promises = [syncChall({ id: syncType.id })];
+                break;
+            case SyncType.TEAM:
+                promises = [syncTeam({ id: syncType.id })];
+                break;
+            case SyncType.USER:
+                promises = [syncUser({ id: syncType.id })];
+                break;
+        }
+
+        const awaitablePromises = promises.map(promise => typeof promise === "function" ? promise() : promise);
+
+        await Promise.all(awaitablePromises);
+        
+        res.statusMessage = "Sync Successful";
+        res.status(200);
+        res.end();
+    } catch (e) {
+        res.status(500);
+        res.end();
+    }
 };
 
 export default handler;
